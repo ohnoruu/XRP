c33a96630457af6c77a278f21446eb095db9f368
RubyD <143292762+ohnoruu@users.noreply.github.com>
2024-06-04 10:14:51 -0700

    Update period2.py

--- period2.py
+++ period2.py
@@ -4,32 +4,33 @@ from XRPLib.motor import Motor
 from XRPLib.timeout import Timeout
 from XRPLib.servo import Servo # arm
 from XRPLib.encoder import Encoder # sensors
+from XRPLib.imu import IMU
+from XRPLib.imu_defs import *
 
-# left motor on 6 and 7 is flipped
-motorL = Motor(6,7,True)
+# DEFINING PINS
+motorL = Motor(6,7,True) # left motor on 6 and 7 is flipped
 encoderL = Encoder(0,4,5) # encoder 0 on pins 4 and 5
-
 motorR = Motor(14,15)
-encoderR = Encoder(1,12,13)
-
+encoderR = Encoder(1,12,13) # encoder 1 on pins 12 and 13
+imu = IMU(19, 18, LSM_ADDR_PRIMARY) # IMU (clock_pin, data_pin, mem_address)
 arm = Servo(16)
 
 wheelDiameter = 6 # cm
 
-def turnRight():
-    trackwidth = 15.5 # cm
-    quarterTurn = 0.25*math.pi*trackwidth
-    while abs(encoderR.get_position())*math.pi*wheelDiameter < quarterTurn: 
-        motorL.set_effort(0.5)
-        motorR.set_effort(-0.5)
-    stop()
+#def turnRight():
+#    trackwidth = 15.5 # cm
+#    quarterTurn = 0.25*math.pi*trackwidth
+#    while abs(encoderR.get_position())*math.pi*wheelDiameter < quarterTurn: 
+#        motorL.set_effort(0.5)
+#        motorR.set_effort(-0.5)
+#    stop()
     
 def turnLeft():
-    trackwidth = 15.5 # cm
-    quarterTurn = 0.25*math.pi*trackwidth
-    while abs(encoderL.get_position())*math.pi*wheelDiameter < quarterTurn: 
+    imu.reset_yaw()
+    while imu.get_yaw() < 90:
         motorL.set_effort(-0.5)
         motorR.set_effort(0.5)
+    imu.reset_yaw()
     stop()
     
 def moveForward(distancecm):
@@ -64,6 +65,7 @@ def moveSquare(sidelength):
         resetEncoders()
         turnLeft()
 
-#stop()
-moveSquare(10)
+moveSquare(20)
 #waveArm()
+
+#turnLeft()
